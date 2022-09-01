63aba49587f95142fac27dcbafa9fa5711478be9
akukamil <chamilioner_kid@mail.ru>
2022-09-01 18:23:32 +0300

    2525

--- index.js
+++ index.js
@@ -4148,7 +4148,7 @@ async function init_game_env(l) {
 	
 			
 	//номер комнаты
-	if (my_data.rating > 1400)
+	if (my_data.rating > 1420)
 		room_name= 'states2';			
 	else
 		room_name= 'states';			
