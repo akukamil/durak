6fdd06fc4e61a864e94cd0a5498bebfbaa987f95
akukamil <chamilioner_kid@mail.ru>
2024-01-25 10:31:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -345,6 +345,7 @@ chat={
 	recent_msg:[],
 	moderation_mode:0,
 	block_next_click:0,
+	kill_next_click:0,
 	payments:0,
 	
 	activate() {	
@@ -481,18 +482,21 @@ chat={
 		
 		if (this.moderation_mode){
 			console.log(player_data.index,player_data.uid,player_data.name.text,player_data.msg.text);
-			
 			fbs_once('players/'+player_data.uid+'/games').then((data)=>{
 				console.log('сыграно игр: ',data)
 			})
-			if (this.block_next_click){
-				fbs.ref('blocked/'+player_data.uid).set(Date.now())
-				fbs.ref('inbox/'+player_data.uid).set({message:'CLIEND_ID',tm:Date.now(),client_id:999999});
-				console.log('Игрок заблокирован и убит: ',player_data.uid);
-				this.block_next_click=0;
-			}			
+		}
 		
-			return
+		if (this.block_next_click){			
+			fbs.ref('blocked/'+player_data.uid).set(Date.now())
+			console.log('Игрок заблокирован: ',player_data.uid);
+			this.block_next_click=0;
+		}
+		
+		if (this.kill_next_click){			
+			fbs.ref('inbox/'+player_data.uid).set({message:'CLIEND_ID',tm:Date.now(),client_id:999999});
+			console.log('Игрок убит: ',player_data.uid);
+			this.kill_next_click=0;
 		}
 		
 		if (objects.chat_keyboard_cont.visible)		
