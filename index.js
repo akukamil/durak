0c131f4fb0c3eaa9bca995b37d55a0367626af73
akukamil <chamilioner_kid@mail.ru>
2024-06-21 12:56:23 +0300

    123

--- index.js
+++ index.js
@@ -4837,7 +4837,7 @@ async function define_platform_and_language() {
 	
 	let s = window.location.href;
 	
-	if (s.includes('yandex.')) {		
+	if (s.includes('yandex.')||s.includes('app-id=194151')) {		
 		game_platform = 'YANDEX';
 		return;
 	}
