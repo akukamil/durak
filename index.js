3efa9bf87a2ed9db4805e696c7c7f3246fb50085
akukamil <chamilioner_kid@mail.ru>
2024-10-21 14:01:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5086,7 +5086,7 @@ async function init_game_env(l) {
 	snow.init();	
 	
 	//номер комнаты
-	let rooms_ranges = [0,1470,1660,9999]
+	let rooms_ranges = [0,1400,1539,9999]
 	if (my_data.rating > rooms_ranges[0] && my_data.rating <= rooms_ranges[1])
 		room_name= 'states';			
 	if (my_data.rating > rooms_ranges[1] && my_data.rating <= rooms_ranges[2])
