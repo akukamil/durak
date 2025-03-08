14909fc9529b8b575962035ba1a05fcc426737e8
akukamil <chamilioner_kid@mail.ru>
2025-03-08 08:20:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5623,11 +5623,9 @@ async function init_game_env(l) {
 	//создаем приложение пикси и добавляем тень
 	const dw=M_WIDTH/document.body.clientWidth;
 	const dh=M_HEIGHT/document.body.clientHeight;
-	const resolution=Math.max(1,Math.min(dw,dh));	
+	const resolution=Math.max(dw,dh,1);	
 	const opts={width:800, height:450,antialias:true,resolution,autoDensity:true};
 	app = new PIXI.Application(opts);
-
-
 	document.body.appendChild(app.renderer.view).style["boxShadow"] = "0 0 15px #000000";
 	document.body.style.backgroundColor = 'rgb(141,211,200)';
 	
