4f648d93f2f651f58e1f832e29906a25ed8b0959
akukamil <chamilioner_kid@mail.ru>
2025-11-10 22:36:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5407,7 +5407,7 @@ lobby={
 		}		
 		
 		//номер комнаты в зависимости от рейтинга игрока
-		const rooms_bins=[0,1396,1460,1600,9999]
+		const rooms_bins=[0,1376,1460,1600,9999]
 		for (let i=1;i<rooms_bins.length;i++){
 			const f=rooms_bins[i-1];
 			const t=rooms_bins[i];
